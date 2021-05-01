53d48bfaa05f31e5301cffee60bf66cf3688c9c3
rvigneshw <vigneshw1811@gmail.com>
2021-05-01 10:51:49 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const muhammara = require('muhammara');
+
+const fromBufferToBuffer = (buffer, passwordOptions) => {
+    try {
+        const readStream = new muhammara.PDFRStreamForBuffer(buffer);
+        const writeStream = new muhammara.PDFWStreamForBuffer();
+        muhammara.recrypt(readStream,writeStream,passwordOptions);
+        return writeStream.buffer;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
+
+const fromFileToBuffer = (inputFilePath, passwordOptions) => {
+    try {
+        const readStream = new muhammara.PDFRStreamForFile(inputFilePath);
+        const writeStream = new muhammara.PDFWStreamForBuffer();
+        muhammara.recrypt(readStream,writeStream,passwordOptions);
+        return writeStream.buffer;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
+
+const fromFileToFile = (inputFilePath, outputFilePath, passwordOptions) => {
+    try {
+        const readStream = new muhammara.PDFRStreamForFile(inputFilePath);
+        const writeStream = new muhammara.PDFWStreamForFile(outputFilePath);
+        muhammara.recrypt(readStream,writeStream,passwordOptions);
+        return outputFilePath;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
+
+const fromBufferTofile = (buffer, outputFilePath, passwordOptions) => {
+    try {
+        const readStream = new muhammara.PDFRStreamForBuffer(buffer);
+        const writeStream = new muhammara.PDFWStreamForFile(outputFilePath);
+        muhammara.recrypt(readStream,writeStream,passwordOptions);
+        return outputFilePath;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
+
+export {
+    fromBufferToBuffer,
+    fromBufferTofile,
+    fromFileToFile,
+    fromFileToBuffer
+}
